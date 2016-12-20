e0b0f4955c35bf83ed2879a106ce5aae054b047f
colby <colby@10.1.10.3>
2016-12-19 18:51:37 -0600

    Made copy-paste modifications as requested

--- Lab12/src/lab12/CoffeeMachine.java
+++ Lab12/src/lab12/CoffeeMachine.java
@@ -3,50 +3,50 @@ package lab12;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.text.DecimalFormat;
+//Janet Leu
 
-//Sammy Student
-public class CoffeeMachine extends JFrame implements ActionListener {
-
+public class CoffeeMachine extends JFrame implements ActionListener
+    {
     private static final long serialVersionUID = 1L;
     JLabel l1, l2, l3, l4, l5, l6;
     JButton b1, b2;
     JTextField t1, t2, t3;
     JCheckBox cream, raw, espresso;
     private JRadioButton small;
+    private JRadioButton medium;
     private JRadioButton large;
     private ButtonGroup group;
-
-    CoffeeMachine() {
+    DecimalFormat df = new DecimalFormat("##,###.00");
+    CoffeeMachine()
+        {
         l1 = new JLabel(" Customer Name");
         l2 = new JLabel(" amount to pay");
         l3 = new JLabel("  ");
         l4 = new JLabel("  ");
         l5 = new JLabel("  ");
         l6 = new JLabel("  ");
-
         b1 = new JButton("COMPUTE");
         b2 = new JButton("EXIT");
-
         t1 = new JTextField(10);
         t2 = new JTextField(10);
-
         small = new JRadioButton("small", true);
+        medium = new JRadioButton("medium", false);
         large = new JRadioButton("large", false);
         group = new ButtonGroup();
         group.add(small);
+        group.add(medium);
         group.add(large);
-
         cream = new JCheckBox("cream", false);
         raw = new JCheckBox("raw sugar", false);
         espresso = new JCheckBox("espresso shot", false);
-
         add(l1);
         add(t1);
         add(small);
         add(cream);
-        add(large);
+        add(medium);
         add(raw);
-        add(l3);
+        add(large);
         add(espresso);
         add(l2);
         add(t2);
@@ -56,43 +56,59 @@ public class CoffeeMachine extends JFrame implements ActionListener {
         add(b2);
         b1.addActionListener(this);
         b2.addActionListener(e -> System.exit(0));
-        setSize(500, 300);
-        setLayout(new GridLayout(7, 2));
+        setSize(500,300);
+        setLayout(new GridLayout(7,2));
         setTitle("Coffee Machine");
-    }
-
-    public void actionPerformed(ActionEvent ae) {
-        float price = 0;
+        }
+    public void actionPerformed(ActionEvent ae)
+        {
+        double price = 0.0;
         String a = "", message = "";
-        if (cream.isSelected() == true) {
+        if (cream.isSelected() == true)
+            {
+            price += 0.00;
+            message="You selected cream";
+            }
+        if (raw.isSelected() == true)
+            {
+            price += 0.00;
+            message +="\nYou selected raw";
             // perform a task ...
-        }
-        if (raw.isSelected() == true) {
+            }
+        if (espresso.isSelected() == true)
+            {
+            price += 0.75;
+            message +="\nYou selected espresso";
             // perform a task ...
-        }
-        if (espresso.isSelected() == true) {
-            // perform a task ...
-        }
-        if (small.isSelected() == true) {
+            }
+        if (small.isSelected() == true)
+            {
             System.out.println("small");
+            message +="\nSmall coffee";
+            price += 1.25;  
             // perform a task ...
-        }
-        if (large.isSelected() == true) {
+            }
+        if (medium.isSelected()== true)
+            {
+            System.out.println("medium");
+            message +="\nMedium coffee";
+            price += 1.75;
+            //perform a task...
+            }   
+        if (large.isSelected() == true)
+            {
             System.out.println("large");
+            message +="\nLarge coffee";
+            price += 2.30;
             // perform a task ...
-        }
-        if (ae.getSource() == b1) {
-            a = t1.getText();
-            message = "your order: " + a + " $" + price;
-            t2.setText("thank you: " + a);
-        }
-        JOptionPane.showMessageDialog(null, "Summary: \n" + message,
+            }  
+        if(ae.getSource() == b1)
+            {
+            message += "\nyour total is: " + a + " $" + df.format(price);
+            t2.setText(df.format(price));
+            }
+        JOptionPane.showMessageDialog(null, "Hello! Enjoy your beverage! \n"
+            + message,
                 "Order Summary", JOptionPane.PLAIN_MESSAGE);
-    }
-
-    public static void main(String args[]) {
-        CoffeeMachine a = new CoffeeMachine();
-        a.setVisible(true);
-        a.setLocation(200, 200);
-    }
+        }
 }
---
+++ Lab12/src/lab12/CoffeeMachineTest.java
@@ -0,0 +1,17 @@
+package lab12;
+
+import lab12.CoffeeMachine;
+import javax.swing.JOptionPane;
+//Colby Underhill
+public class CoffeeMachineTest 
+    {
+    public static void main(String[] args) 
+        {
+        {
+        CoffeeMachine a = new CoffeeMachine();
+        a.setVisible(true);
+        a.setLocation(200,200);
+        }
+        }
+    
+    }
